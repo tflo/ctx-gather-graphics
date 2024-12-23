8e777346b3368006b6a49e5f944699f7c060ce95
6mot, Tom <tflo@users.noreply.github.com>
2024-12-23 19:19:42 +0100

    Removed comments, changed var names

--- ctx-gather-graphics.lua
+++ ctx-gather-graphics.lua
@@ -3,16 +3,10 @@
 
 -- Version 0.9 (2024-12-23)
 
--- REQUIREMENTS:
--- macOS
--- A `...figures-usage.lua` file must be in the same directory as the script.
--- This file will be created by Ctx when you compile with `\enabletrackers[graphics.usage]` in an early environment, or in the compilation file.
--- Lua, probably 5.1 or later.
--- This script must be run from the same directory where the compilation file is located.
--- Set the variable `destdir` to the destination directory where you want to gather the figure files. It will be created if it doesn't exist yet.
+-- Set the variable `destdir` to the destination directory where you want to gather the graphic files. It will be created if it doesn't exist yet.
 -- Path can be absolute (example 1), or relative (example 2) to the script location. Spaces should be OK.
-local destdir = '$HOME/_tmp/Ctx Gathered Figs/BA P3.0-de' -- Absolute path
--- local destdir = '../gathered_figs' -- Relative path
+local destdir = '$HOME/_tmp/Ctx Gathered Figs/BA P3.0-de' -- Absolute path example.
+-- local destdir = '../gathered_figs' -- Relative path example.
 
 -- Lua script BEGIN
 -- Let's identify the '...figures-usage.lua' file in the current directory (usually in _compile.nosync.nobackup).
@@ -21,20 +15,20 @@ local tmp_output = os.tmpname()
 -- The shell cmd arg is always a single string, so we can just concatenate.
 os.execute("find . -type f -name '*figures-usage.lua' > " .. tmp_output)
 -- Read the path from the content of our tmp file and delete the file.
-local figusagetable_path
+local graphicstable_path
 for line in io.lines(tmp_output) do
-	figusagetable_path = line
+	graphicstable_path = line
 	break -- There should be only one line.
 end
 os.remove(tmp_output)
 
-if not figusagetable_path then -- Check if `find` did actually find the file.
-	print 'No `*figures-usage.lua` file found! Aborting.'
+if not graphicstable_path then -- Check if `find` did actually find the file.
+	print 'No `*-figures-usage.lua` file found! Aborting.'
 	return
 end
 
--- Read the table from the figusagetable file.
-local figusagetable = dofile(figusagetable_path)
+-- Read the table from the graphicstable file.
+local graphicstable = dofile(graphicstable_path)
 -- Set the copy cmd we want to use (`-c` ensures that `cp` makes APFS clones (macOS!)).
 local copycmd = 'cp -c'
 -- Create our destination dir. If it already exists, it's OK too.
@@ -46,16 +40,16 @@ if not dircreated then -- Check for success (`mkdir` may fail because of permiss
 	return
 end
 -- Build a new table consisting only of the paths.
-local figpaths = {}
-for _, v in ipairs(figusagetable.found) do
-	table.insert(figpaths, v.foundname)
+local graphicpaths = {}
+for _, v in ipairs(graphicstable.found) do
+	table.insert(graphicpaths, v.foundname)
 	-- If `foundname` is different from `fullname`, we want both paths. See note at the end.
 	if v.foundname ~= v.fullname then
-		table.insert(figpaths, v.fullname)
+		table.insert(graphicpaths, v.fullname)
 	end
 end
 -- Now copy all the paths from the table into our destination dir.
-for _, path in ipairs(figpaths) do
+for _, path in ipairs(graphicpaths) do
 	os.execute(string.format('%s "%s" "%s"', copycmd, path, destdir))
 end
 print 'Script finished, probably successfully.'
